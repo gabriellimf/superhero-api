7618ac5fd9bb675436aba52542650a9325f64b5b
gabriellimf <gabriellimombrinii@gmail.com>
2024-06-17 09:25:42 -0300

    feat: Update README.md and docker-compose.yml, add AuthRequest DTO

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,16 +2,18 @@ import { Controller, UseGuards, Post, Req } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { AuthService } from './auth.service';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { AuthRequest } from './dtos/auth-request';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
-  @ApiResponse({ status: 200, description: 'The user has been successfully logged in.' })
-  async login(@Req() req: any) {
+  @ApiResponse({ status: 200, description: 'Login' })
+  async login(@Req() req: AuthRequest) {
     return await this.authService.login(req.user);
   }
 }
